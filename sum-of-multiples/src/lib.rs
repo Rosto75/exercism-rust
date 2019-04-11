2fc4025821b14138525b76e917ea8f598e82b2b1
Tomasz Różański <rosto@tlen.pl>
2019-04-12 01:34:29 +0200

    Change 'sum-of-multiplies' solution to use my own code

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-pub fn sum_of_multiples2(limit: u32, factors: &[u32]) -> u32 {
+pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
     let mut map = HashMap::new();
 
     for num in 1..limit {
@@ -14,7 +14,10 @@ pub fn sum_of_multiples2(limit: u32, factors: &[u32]) -> u32 {
     map.iter().map(|(k, _)| k).sum()
 }
 
-pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
+// This version is modified version of someone elses solution:
+// https://exercism.io/tracks/rust/exercises/sum-of-multiples/solutions/71f7a08fcac34202bec795163eee2697
+// I added a zero check, so it could pass one of the tests.
+pub fn sum_of_multiples2(limit: u32, factors: &[u32]) -> u32 {
     (1..limit)
         .filter(|i| factors.iter().any(|f| *f != 0 && i % f == 0))
         .sum()
